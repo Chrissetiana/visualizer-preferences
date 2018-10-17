78611bc5118299487bc3c7524687aac29d9506e0
chrissetiana <chrissetiana@gmail.com>
2018-10-17 01:05:31 +0100

    Add settings fragment

---
+++ app/src/main/java/com/chrissetiana/visualizationthings/SettingsFragment.java
@@ -0,0 +1,12 @@
+package com.chrissetiana.visualizationthings;
+
+import android.os.Bundle;
+import android.support.v7.preference.PreferenceFragmentCompat;
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+
+    @Override
+    public void onCreatePreferences(Bundle bundle, String s) {
+        addPreferencesFromResource(R.xml.pref_visualizer);
+    }
+}
